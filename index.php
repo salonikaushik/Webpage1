75932d4bd76c9faf05639c70f839524d1429c68c
salonikaushik <salonisingh455275@gmail.com>
2019-03-18 12:07:37 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li>About</li>
+                <li>Team</li>
+                <li>Work</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    </header>
+
+    <section>
+        <div class="container">     
+            <h1>WE ARE BROADWAY</h1>
+                <div class="get_started"><div class="button"><a href="#">GET STARTED</a></div></div>
+        </div>
+    </section>
+
+    <section class="section-container">
+        <div class="section2">
+            <div class="image">
+                <img src="./Demo_pencil.png">
+                <h2>DESIGN</h2>
+                <p>Make your projects look great and interact beautifully.</p>
+                <a href="#">LEARN MORE</a>
+            </div>
+
+             <div class="image">
+                <img src="./img2.png"/>
+                <h2>DEVELOP</h2>
+                <p>Use modern tools to turn your design into a web site</p>
+                <a href="#">LEARN MORE</a>
+            </div>
+
+            <div class="image">
+                <img src="./img3.png"/>
+                <h2>DEPLOY</h2>
+                <p>Use modern tools to turn your design into a web site</p>
+                <a href="#">LEARN MORE</a>
+            </div>
+        </div>  
+    </section>
+    <footer class="footer">
+        <p>@BROADWAY 2017</p>
+
+    </footer>
+    
+</body>
+</html>
